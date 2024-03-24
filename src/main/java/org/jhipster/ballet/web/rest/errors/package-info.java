360a42ce93290ea0bffc2addaa6adbc729bfb267
Elodie Collier <egcollier19@gmail.com>
2024-03-24 12:34:03 -0400

    Initial version of ballet generated by generator-jhipster@7.9.3

---
+++ src/main/java/org/jhipster/ballet/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package org.jhipster.ballet.web.rest.errors;
---
+++ src/test/java/org/jhipster/ballet/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.jhipster.ballet;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.jhipster.ballet.BalletApp;
+import org.jhipster.ballet.config.AsyncSyncConfiguration;
+import org.jhipster.ballet.config.EmbeddedSQL;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BalletApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
